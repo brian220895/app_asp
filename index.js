06255926c6d75e29548d1cce3ec7442111e6da7f
brian <trmthanh220895@gmail.com>
2023-08-09 16:18:10 +0700

    Thanh cong rr

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-routers(app)
+// routers(app)
 // app.use('/posts', posts);
 
 app.get('/posts',(req,res)=>{
