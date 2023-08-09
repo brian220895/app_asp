ef15b77713a363d164fb88102a7a532bb1dfe011
brian <trmthanh220895@gmail.com>
2023-08-09 14:56:57 +0700

    first commit 5

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import routers from './routes/index.js';
+// import mongoose from 'mongoose'
+// import routers from './routes/index.js';
 dotenv.config();
 var app = express()
 var PORT=process.env.PORT
 var URI =process.env.URI
 
-import bodyParser from 'body-parser' 
+// import bodyParser from 'body-parser' 
 
 app.use(cors());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-// import router1 from './apiRouter.js'
-// app.use('/admin/api/v1/', router1)
+
 
 app.use(cors());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-routers(app)
-// app.use('/posts', posts);
-
-mongoose
-  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to DB');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log('err', err);
-  });
+// routers(app)
 
 
+// mongoose
+//   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to DB');
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('err', err);
+//   });
 
+app.get('/', (req,res)=>{
+  res.json('backend')
+})
 
-// app.listen(port, ()=>{
-//     console.log(`Server started on port ${PORT}`)
-// })
+var port=5000
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`)
+})
