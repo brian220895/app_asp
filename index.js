686541260ae85a37a218e9e0446956b9deae6cb6
brian <trmthanh220895@gmail.com>
2023-08-09 16:50:50 +0700

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import routers from './routes/index.js';
 dotenv.config();
 var app = express()
 var port=process.env.PORT ||5000
-var url =process.env.URI
+var url =process.env.CYCLIC_DB
 
 // import bodyParser from 'body-parser' 
 
@@ -31,7 +31,7 @@ routers(app)
 
 
 mongoose
-  .connect( url || "mongodb+srv://trmthanh220895:d2zi3piX30pKVXpx@cluster05072023.8wowfby.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect( url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to DB');
     app.listen(port, () => {
