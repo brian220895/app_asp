6a1bc49f5acaef57e0c467c557c0c67c135c0f34
brian <trmthanh220895@gmail.com>
2023-08-31 21:12:23 +0700

    sdfsdfsdkkkkkk

--- index.js
+++ index.js
@@ -13,18 +13,34 @@ var url =process.env.URI
 app.use(cors());
 
 
-// const corsOptions = {
-//   credentials: true,
-//   ///..other options
-// };
+const corsOptions = {
+  credentials: true,
+  ///..other options
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+app.use(cors({
+  origin:[
+    'https://brian-server.cyclic.app',
+    
+  ],
+  credentials:true,
+  method:['GET','PUT','POST','DELETE','OPTIONS'],
+  allowedHeaders:[
+    'Access-Control-Allow-Origin',
+    'Content-Type',
+    'Authorization',
+  ],
+}));
+
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // app.use("/",createProxyMiddleware({ 
 //     target: 'https://brian-server.cyclic.app', //original url
