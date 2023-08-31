4c1fb012bd35fe9dc476d851e340d0fb98998390
brian <trmthanh220895@gmail.com>
2023-08-31 21:01:50 +0700

    ssttttttttt

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ var app = express()
 var port=process.env.PORT ||3001
 var url =process.env.URI
 
-// app.use(cors());
+app.use(cors());
 
 
-const corsOptions = {
-  credentials: true,
-  ///..other options
-};
+// const corsOptions = {
+//   credentials: true,
+//   ///..other options
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
