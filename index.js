3399b16db73c57dafa3cd7d763ec3b3ff35d20d1
brian <trmthanh220895@gmail.com>
2023-08-31 12:33:21 +0700

    first commit 23

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ var app = express()
 var port=process.env.PORT ||3001
 var url =process.env.URI
 
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin:[
+    'http://localhost:3000',
+    'https://brian-server.cyclic.app'
+  ],
+  credentials:true,
+  method:['GET','PUT','POST','DELETE','OPTIONS'],
+  allowedHeaders:[
+    'Access-Control-Allow-Origin',
+    'Content-Type',
+    'Authorization',
+  ],
+}));
+
+
 app.use(cookieParser())
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
