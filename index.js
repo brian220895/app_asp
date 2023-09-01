46c7c09a06382d2a817295b44615df0d904599ed
brian <trmthanh220895@gmail.com>
2023-09-01 15:35:02 +0700

    555

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ var url =process.env.URI
 
 
 
-
-app.use(cookieParser())
+app.use(express.cookieParser());
+// app.use(cookieParser())
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
