8e4c081ab95e7ac26de5b8b435b49ab71c94b63c
brian <trmthanh220895@gmail.com>
2023-09-01 15:44:12 +0700

    ty 8

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var url =process.env.URI
 
 
 
-app.use(express.cookieParser());
+// app.use(express.cookieParser());
 // app.use(cookieParser())
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
