1647e20051609a9bb671176a386531b4ea2ede5e
brian <trmthanh220895@gmail.com>
2023-09-01 21:34:35 +0700

    dattsdfdddd

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -208,7 +208,8 @@ export const loginUser = async (req, res) => {
 
             //   res.cookie('sitesSecurity', 'anonystick.com', {signed: true})
             //   res.json({ok: 1})
-            response.cookie('cookie2', 'value2', { sameSite: 'none', secure: true });
+            // {signed: true}
+            response.cookie('cookie2', 'value2', { sameSite: 'none', secure: true,signed: true });
             res.send();
             //   res.cookie("token", accessToken, {
             //     // httpOnly: true,
