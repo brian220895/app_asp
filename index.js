749e283c514b740ac8c515a8a6428283ba5821b3
brian <trmthanh220895@gmail.com>
2023-09-02 12:28:04 +0700

    them 3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express()
 var port=process.env.PORT ||3001
 var url =process.env.URI
 
-app.use(cors());
+// app.use(cors());
 
 // app.use(cookieParser('anonystick'));
 
@@ -21,10 +21,10 @@ app.use(cookieParser())
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
-// app.use("*",cors({
-//   origin:true,
-//   credentials:true
-// }))
+app.use("*",cors({
+  origin:true,
+  credentials:true
+}))
 
 routers(app)
 
