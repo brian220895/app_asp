e3aaed60e1b9cd543ddbfb87ad06c45475f88394
brian <trmthanh220895@gmail.com>
2023-09-02 08:06:32 +0700

    vvvvvvv

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ routers(app)
 
 
 app.get('/setcookie', (req, res)=> {
-    // res.cookie('sitesSecurity', 'anonystick.com', {signed: true})
-    // res.json({ok: 1})
-    cookies.set('myCat', 'Pacman', { domain:thegioimauxanh.com,sameSite: "none", path: '/' });
-    // console.log(cookies.get('myCat222'));
+    res.cookie('sitesSecurity', 'anonystick.com', {signed: true})
+    res.json({ok: 1})
+    // cookies.set('myCat', 'Pacman', { domain:thegioimauxanh.com,sameSite: "none", path: '/' });
+    // // console.log(cookies.get('myCat222'));
 })
 
 app.get('/getcookie', (req, res)=> {
