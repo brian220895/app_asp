123cbfdcc47d6834f913f9efaafffe7b7a7df21a
brian <trmthanh220895@gmail.com>
2023-09-02 13:18:09 +0700

    Them 15

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -206,27 +206,13 @@ export const loginUser = async (req, res) => {
               const accessToken = generateAccessToken(checkUser)
               const refreshToken = generateRefreshToken(checkUser)
  
-            //   res.cookie("token", accessToken, {
-            //     httpOnly: true,
-            //     secure:true,
-            //     // path: "/",
-            //     sameSite: "strict",
-            //   });
-      
-     
-            
-            //   res.cookie("refreshToken", refreshToken, {
-            //     httpOnly: true,
-            //     secure:true,
-            //     // path: "/",
-            //     sameSite: "strict",
-            //   });
               const {password,...others}=checkUser._doc
               // console.log(checkUser._doc)
               return res.status(200).json({
                   status: 'OK',
                   message: 'SUCCESS',
                   accessToken,
+                  refreshToken,
                   ...others
               })
             }
