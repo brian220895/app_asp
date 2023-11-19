6a3b6f0df1a518b7d8dd37d7a40084e5174236b4
brian220895 <trmthanh220895@gmail.com>
2023-11-20 00:56:06 +0700

    ada

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ var app = express()
 var port=process.env.PORT ||3001
 var url =process.env.URI
 
-// var  corsOptions  = {
-//   origin:'https://peaceful-sunflower-941f16.netlify.app', //frontend url
-//   credentials: true,
-//   allowedHeaders:'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//   methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   exposedHeaders:'Content-Range, X-Content-Range'
-// }
+var  corsOptions  = {
+  origin:'https://thegioimauxanh.com', //frontend url
+  credentials: true,
+  allowedHeaders:'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
+  exposedHeaders:'Content-Range, X-Content-Range'
+}
 
 // Value in corsOptions must be a string ['GET,HEAD,PUT,PATCH,POST,DELETE'], ['GET','HEAD','PUT','PATCH','POST','DELETE'],
 // 'GET,HEAD,PUT,PATCH,POST,DELETE'
@@ -28,22 +28,9 @@ var url =process.env.URI
 //    headers: {'Content-Type': 'application/json',}
 // };
   
-//  app.use(cors(corsOptions));
+ app.use(cors(corsOptions));
 
 
-var allowlist = ['https://peaceful-sunflower-941f16.netlify.app', 'https://thegioimauxanh.com']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
-
-app.use(cors(corsOptionsDelegate));
 
 
 app.use(express.json({ limit: '30mb' }));
