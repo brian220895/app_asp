3e8571ec3c16a1313d41ab056c17c6d60b6b36c4
brian220895 <trmthanh220895@gmail.com>
2023-11-19 23:59:36 +0700

    tha

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var url =process.env.URI
 
 
 var  corsOptions  = {
-  origin:'https://www.thegioimauxanh.com', //frontend url
+  origin:'https://thegioimauxanh.com', //frontend url
   credentials: true,
   allowedHeaders:'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
